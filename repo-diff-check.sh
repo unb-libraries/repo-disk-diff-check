b2b0d85e2f4c37d3b590ee10d3ef3964a308d59c
Chaudhry Usman Ali <mani.ali@unb.ca>
2013-06-12 12:49:44 -0300

    Corrected code for checking against files

--- repo-diff-check.sh
+++ repo-diff-check.sh
@@ -3,12 +3,16 @@
 #tmp_to_check= "/tmp/relentless-etc-compare-files/etc"
 #file-from-etc ="/var/opt/repos/relentless-etc-files"
 tmp_to_check = $1
-file-from-etc = $2
+files-from-etc = $2
+files-to-check = $3
+
 rm -rf /tmp/$tmp_to_check
 mkdir /tmp/$tmp_to_check
-git clone --recursive $file-from-etc $tmp_to_check
+
+git clone --recursive $files-from-etc $tmp_to_check
+
 for f in $tmp_to_check
-	for g in $file-from-etc
+	for g in $files-to-check
 do
 	DIFF = $DIFF$(diff f g)
 done
@@ -17,4 +21,6 @@ if["$DIFF" != ""]
 	then
 	echo "There are some differences"
 	/var/opt/ec2-sns-sender/sns_send -t arn:aws:sns:us-east-1:344420214229:unb_lib_git_pushes -s '[Relentless][etc Files][PUSH]' -m "$DIFF" --subject "DIFF Files have changed"
+else
+	sudo cp -R $tmp_to_check /$files-to-check
 fi
