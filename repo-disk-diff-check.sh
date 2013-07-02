729b92e4567e653b7941dabc4184083e3dadc67d
Brian Cassidy <bricas@cpan.org>
2013-07-02 15:43:31 -0300

    Change to unified diff output

--- repo-disk-diff-check.sh
+++ repo-disk-diff-check.sh
@@ -10,7 +10,7 @@ git clone --quiet --recursive $repository_path $temp_checkout_path
 temp_checkout_files=$(find $temp_checkout_path -type f | grep -v '\/\.git\/')
 for checked_out_file in $temp_checkout_files; do
         deployed_file=$(echo $checked_out_file | sed -e 's|'$temp_checkout_path'|'$deploy_path'|g')
-        deployed_diff=$(diff $checked_out_file $deployed_file | sed -e 's/^\s*//g' -e 's/(\s|\n)*$//g' )
+        deployed_diff=$(diff -u $checked_out_file $deployed_file | sed -e 's/^\s*//g' -e 's/(\s|\n)*$//g' )
         if [ "$deployed_diff" ]
         then
                 relative_filepath=$(echo $checked_out_file | sed -e 's|'$temp_checkout_path'/||g')
