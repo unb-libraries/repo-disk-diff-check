92d1ed9d2f889960a98ecc69e0d78c0138e37a0a
Jacob Sanford <jsanford@unb.ca>
2014-10-29 10:50:37 -0300

    Quote unquoteds

--- repo-disk-diff-check.sh
+++ repo-disk-diff-check.sh
@@ -3,17 +3,17 @@ temp_checkout_path="/tmp/$$"
 repository_path=$1
 deploy_path=$2
 
-rm -rf $temp_checkout_path
-mkdir $temp_checkout_path
-git clone --quiet --recursive $repository_path $temp_checkout_path > /dev/null
+rm -rf "$temp_checkout_path""
+mkdir "$temp_checkout_path""
+git clone --quiet --recursive "$repository_path" "$temp_checkout_path" > /dev/null
 
-temp_checkout_files=$(find $temp_checkout_path -type f | grep -v '\/\.git\/')
+temp_checkout_files=$(find "$temp_checkout_path" -type f | grep -v '\/\.git\/')
 for checked_out_file in $temp_checkout_files; do
-        deployed_file=$(echo $checked_out_file | sed -e 's|'$temp_checkout_path'|'$deploy_path'|g')
-        deployed_diff=$(diff -u $checked_out_file $deployed_file | sed -e 's/^\s*//g' -e 's/(\s|\n)*$//g' )
+        deployed_file=$(echo "$checked_out_file" | sed -e 's|'$temp_checkout_path'|'$deploy_path'|g')
+        deployed_diff=$(diff -u "$checked_out_file" "$deployed_file" | sed -e 's/^\s*//g' -e 's/(\s|\n)*$//g' )
         if [ "$deployed_diff" ]
         then
-                relative_filepath=$(echo $checked_out_file | sed -e 's|'$temp_checkout_path'/||g')
+                relative_filepath=$(echo "$checked_out_file" | sed -e 's|'$temp_checkout_path'/||g')
                 output_block="$output_block
 $relative_filepath:
 $deployed_diff
@@ -22,7 +22,7 @@ $deployed_diff
         fi
 done
 
-rm -rf $temp_checkout_path
+rm -rf "$temp_checkout_path""
 
 if [ "$output_block" ]
 then
