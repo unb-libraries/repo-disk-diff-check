5aa08059489d2af703dce553ce3f8946c845efd9
Brian Cassidy <brian.cassidy@gmail.com>
2014-10-30 10:22:16 -0300

    change to use our sns_sender client

--- repo-disk-diff-check.py
+++ repo-disk-diff-check.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python
 
-import boto
 import subprocess
+import os
 import socket
 import tempfile
 import shutil
 import repos as repos
 
-def send_sns_msg_aws(topic_arn, mesg, topicstring):
-    try:
-        c = boto.connect_sns()
-        c.publish(topic_arn, mesg, topicstring)
-    except Exception , e:
-        print e
-
+ec2_sns_sender_binpath = '/var/opt/ec2-sns-sender/sns_send'
 hostname = socket.gethostname()
 
-for cur_repo in repos.repos_to_check:
+def send_sns(topic_id, subject, message):
+    DEVNULL = open(os.devnull, 'w')
+    subprocess.call([ ec2_sns_sender_binpath, '-t', topic_id, '-s', subject, '-m', message ], stdout=DEVNULL, stderr=DEVNULL)
+
+for repo in repos.repos_to_check:
     tempdir = tempfile.mkdtemp()
-    subprocess.call([ 'git', 'clone', '--quiet', '--recursive', cur_repo['repo-path'], tempdir ])
-    diff_output = subprocess.check_output([ 'git', '--git-dir=' + tempdir + '/.git', '--work-tree=' + cur_repo['deploy-path'], 'diff' ])
+    subprocess.call([ 'git', 'clone', '--quiet', '--recursive', repo['repo-path'], tempdir ])
+    diff_output = subprocess.check_output([ 'git', '--git-dir=' + tempdir + '/.git', '--work-tree=' + repo['deploy-path'], 'diff' ])
     shutil.rmtree(tempdir)
 
     if diff_output:
-        subject = 'WARNING : Uncommitted Changes in ' + cur_repo['repo-path'] + ' on ' + hostname
-        send_sns_msg_aws(
-                         'arn:aws:sns:us-east-1:344420214229:repo_out_of_sync',
-                         diff_output,
-                         subject 
-                        )
-        send_sns_msg_aws(
-                         'arn:aws:sns:us-east-1:344420214229:unb_lib_developers',
-                         subject,
-                         subject 
-                         )
+        subject = 'WARNING : Uncommitted Changes in ' + repo['repo-path'] + ' on ' + hostname
+        send_sns('arn:aws:sns:us-east-1:344420214229:repo_out_of_sync', subject, diff_output)
+        send_sns('arn:aws:sns:us-east-1:344420214229:unb_lib_developers', subject, subject)
